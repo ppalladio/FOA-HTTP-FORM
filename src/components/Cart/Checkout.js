47764a9c37754326e227fa88cb2688625613972c
ppalladio <nine.slide@gmail.com>
2022-10-18 00:52:46 +0200

    validation (tbc)

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,48 +1,66 @@
-import {useRef} from 'react'
+import { useRef } from 'react';
 
 import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim().length === 0;
+const isFiveChar = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalInputRef = useRef();
+    const cityInputRef = useRef();
+    const confirmHandler = (event) => {
+        event.preventDefault();
 
-    const nameInputRef = useRef()
-    const streetInputRef = useRef()
-    const postalInputRef = useRef()
-    const cityInputRef = useRef()
-  const confirmHandler = (event) => {
-    event.preventDefault();
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostal = postalInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
 
-    const enteredName = nameInputRef.current.value
-    const enteredStreet = streetInputRef.current.value
-    const enteredPostal = postalInputRef.current.value
-    const enteredcity = cityInputRef.current.value
-  };
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredCityIsValid = !isEmpty(enteredCity);
+        const enteredPostalIsValid = isFiveChar(enteredPostal);
 
-  return (
-    <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='street'>Street</label>
-        <input type='text' id='street' ref={streetInputRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' ref={postalInputRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='city'>City</label>
-        <input type='text' id='city' ref={cityInputRef}/>
-      </div>
-      <div className={classes.actions}>
-        <button type='button' onClick={props.onCancel}>
-          Cancel
-        </button>
-        <button className={classes.submit}>Confirm</button>
-      </div>
-    </form>
-  );
+        const formIsValid =
+            enteredNameIsValid &&
+            enteredStreetIsValid &&
+            enteredPostalIsValid &&
+            enteredCityIsValid;
+
+            
+        if (!formIsValid) {
+            return;
+        }
+    };
+
+    return (
+        <form className={classes.form} onSubmit={confirmHandler}>
+            <div className={classes.control}>
+                <label htmlFor="name">Your Name</label>
+                <input ref={nameInputRef} type="text" id="name" />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="street">Street</label>
+                <input type="text" id="street" ref={streetInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="postal">Postal Code</label>
+                <input type="text" id="postal" ref={postalInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="city">City</label>
+                <input type="text" id="city" ref={cityInputRef} />
+            </div>
+            <div className={classes.actions}>
+                <button type="button" onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
+        </form>
+    );
 };
 
 export default Checkout;
